5961688c05247b73288fd14397dcdb708a5abb2a
WahyuMutiara <72063869+WahyuMutiara@users.noreply.github.com>
2020-09-30 13:42:12 +0700

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NUGROHO
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+
+ int input;
+        boolean isTrue = false;
+        Scanner user = new Scanner(System.in);
+
+ System.out.println("Masukkan angka -->");
+                input = user.nextInt();
+                if (input % 2 == 0) {
+                    System.out.println(input + " Adalah Bilangan Genap");
+                 
+                } else {
+                    System.out.println(input + " Adalah Bilangan Ganjil");
+                }
+
+
+
+
+}
+}
